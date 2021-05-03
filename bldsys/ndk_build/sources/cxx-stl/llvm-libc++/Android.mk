d6ff9b13c9fc2eb97f041d7742ac2ad8a92da937
HanetakaYuminaga <HanetakaYuminaga@outlook.com>
2021-05-03 09:35:29 +0800

    ndk-build linux64

---
+++ bldsys/ndk_build/sources/cxx-stl/llvm-libc++/Android.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) YuqiaoZhang(HanetakaYuminaga)
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libpt_llvm_cxx_static
+LOCAL_SRC_FILES := $(LOCAL_PATH)/pt_llvm_cxx_fake.cpp
+LOCAL_EXPORT_CPPFLAGS := -std=c++11 -rtlib=compiler-rt -unwindlib=libunwind -stdlib=libc++ #-nostdlib -nodefaultlibs #-static-libgcc -static-libstdc++ #-D_GLIBCXX_USE_CXX11_ABI=1 
+LOCAL_EXPORT_LDFLAGS := -rtlib=compiler-rt -unwindlib=libunwind -stdlib=libc++ #-Wl,-Bstatic -lc++ -lunwind -llzma -Wl,-Bdynamic #-nostdlib -nodefaultlibs #-static-libgcc -static-libstdc++
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libpt_llvm_cxx_shared
+LOCAL_SRC_FILES := $(LOCAL_PATH)/pt_llvm_cxx_fake.cpp
+LOCAL_EXPORT_CPPFLAGS := -std=c++11 -rtlib=compiler-rt -unwindlib=libunwind -stdlib=libc++ #-nostdlib -nodefaultlibs #-static-libgcc -static-libstdc++ #-D_GLIBCXX_USE_CXX11_ABI=1 
+LOCAL_EXPORT_LDFLAGS := -rtlib=compiler-rt -unwindlib=libunwind -stdlib=libc++ #-lc++ -lunwind #-nostdlib -nodefaultlibs #-Wl,-Bstatic -lc++ -lunwind -llzma -Wl,-Bdynamic #-static-libgcc -static-libstdc++
+include $(BUILD_STATIC_LIBRARY)
+
+# TODO
+# enable LOCAL_DISABLE_FATAL_LINKER_WARNINGS in build-binary.mk
+# x86/Gos-linux.c:299: warning: sigreturn is not implemented and will always fail
